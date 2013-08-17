50e94522532a5b708935f82ba723cbb579dcddc8
Michael Nguyen <mn626@mmlab18.cs.uow.edu.au>
2013-08-17 20:23:36 +1000

    Initial Commit

---
+++ a1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  a1
+//
+//  Created by Michael Nguyen on 17/08/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ a1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  a1
+//
+//  Created by Michael Nguyen on 17/08/13.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
