9c498c2e58e7a6cc62a9396793bee4b12a1ce686
Michael Nguyen <mn626@uowmail.edu.au>
2013-08-17 23:15:45 +1000

    initial commit

---
+++ a1/a1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  a1
+//
+//  Created by Michael Nguyen on 17/08/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
